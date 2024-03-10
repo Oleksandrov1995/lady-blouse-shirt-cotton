d55356c0bb59ad9077092eae3855fb7933e785bc
Oleksandrov1995 <olexandrov1995@gmail.com>
2024-03-10 14:13:56 +0100

    create

---
+++ src/components/ProductSize/ProductSize.jsx
@@ -0,0 +1,59 @@
+import './ProductSize.css';
+
+export const ProductSize = () => {
+  return (
+    <section id="productSize" className='productSize'>
+      <h2>Заміри виробу</h2>
+      <table className='productSize-table'>
+  <thead>
+    <tr>
+      <th>Розмір</th>
+      <th>Ваш обхват грудей, см</th>
+      <th>Обхват грудей виробу, см</th>
+      <th>Довжина ззаду, см</th>
+      <th>Довжина спереду, см</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>42-44</td>
+      <td>84-96</td>
+      <td>114</td>
+      <td>78</td>
+      <td>62</td>
+    </tr>
+    <tr>
+    <td>46-48</td>
+      <td>97-106</td>
+      <td>118</td>
+      <td>78</td>
+      <td>62</td>
+    </tr>
+    <tr>
+    <td>50-52</td>
+      <td>107-116</td>
+      <td>124</td>
+      <td>80</td>
+      <td>64</td>
+    </tr>
+    <tr>
+    <td>54-56</td>
+      <td>117-122</td>
+      <td>132</td>
+      <td>83</td>
+      <td>68</td>
+    </tr>
+    <tr>
+    <td>58-60</td>
+      <td>120-135</td>
+      <td>142</td>
+      <td>83</td>
+      <td>69</td>
+    </tr>
+  </tbody>
+</table>
+
+      <p><span className='productSize-bolt'>*</span>   Розміри розраховані сидіти вільно а фасон відповідає сучасним трендам, підходе на всі типи фігур та має широку варіативність подачі образу та об'ємності силуету.</p>
+    </section>
+  );
+};
---
+++ src/data/reviewsData.js
@@ -0,0 +1,86 @@
+export const reviewsData = [
+  {
+    id: 1,
+    imageSrc: require('../Images/reviews/reviews1-min.PNG'),
+  },
+  {
+    id: 2,
+    imageSrc: require('../Images/reviews/reviews2-min.PNG'),
+  },
+  {
+    id: 3,
+    imageSrc: require('../Images/reviews/reviews3-min.PNG'),
+  },
+  {
+    id: 4,
+    imageSrc: require('../Images/reviews/reviews4-min.PNG'),
+  },
+  {
+    id: 5,
+    imageSrc: require('../Images/reviews/reviews5-min.PNG'),
+  },
+  {
+    id: 6,
+    imageSrc: require('../Images/reviews/reviews6-min.PNG'),
+  },
+  {
+    id: 7,
+    imageSrc: require('../Images/reviews/reviews7-min.PNG'),
+  },
+  {
+    id: 8,
+    imageSrc: require('../Images/reviews/reviews8-min.PNG'),
+  },
+  {
+    id: 11,
+    imageSrc: require('../Images/reviews/reviews11-min.PNG'),
+  },
+  {
+    id: 12,
+    imageSrc: require('../Images/reviews/reviews12-min.PNG'),
+  },
+  {
+    id: 13,
+    imageSrc: require('../Images/reviews/reviews13-min.PNG'),
+  },
+  {
+    id: 14,
+    imageSrc: require('../Images/reviews/reviews14-min.PNG'),
+  },
+  {
+    id: 15,
+    imageSrc: require('../Images/reviews/reviews15-min.PNG'),
+  },
+  {
+    id: 16,
+    imageSrc: require('../Images/reviews/reviews16-min.PNG'),
+  },
+  {
+    id: 17,
+    imageSrc: require('../Images/reviews/reviews17-min.PNG'),
+  },
+  {
+    id: 18,
+    imageSrc: require('../Images/reviews/reviews18-min.PNG'),
+  },
+  {
+    id: 19,
+    imageSrc: require('../Images/reviews/reviews19-min.PNG'),
+  },
+  {
+    id: 20,
+    imageSrc: require('../Images/reviews/reviews20-min.PNG'),
+  },
+  {
+    id: 21,
+    imageSrc: require('../Images/reviews/reviews21-min.PNG'),
+  },
+  {
+    id: 22,
+    imageSrc: require('../Images/reviews/reviews22-min.PNG'),
+  },
+  {
+    id: 23,
+    imageSrc: require('../Images/reviews/reviews23-min.PNG'),
+  },
+];
