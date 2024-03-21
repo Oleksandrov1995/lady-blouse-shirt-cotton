20f5d85545c182019a2a1b5a23ebcfcc0072591c
Oleksandrov1995 <olexandrov1995@gmail.com>
2024-03-21 12:06:35 +0100

    Update reviewsData.js

--- src/data/reviewsData.js
+++ src/data/reviewsData.js
@@ -3,84 +3,84 @@ export const reviewsData = [
   //   id: 1,
   //   imageSrc: require('../Images/reviews/reviews1-min.png'),
   // },
-  {
-    id: 2,
-    imageSrc: require('../Images/reviews/reviews2-min.png'),
-  },
-  {
-    id: 3,
-    imageSrc: require('../Images/reviews/reviews3-min.png'),
-  },
-  {
-    id: 4,
-    imageSrc: require('../Images/reviews/reviews4-min.png'),
-  },
-  {
-    id: 5,
-    imageSrc: require('../Images/reviews/reviews5-min.png'),
-  },
-  {
-    id: 6,
-    imageSrc: require('../Images/reviews/reviews6-min.png'),
-  },
-  {
-    id: 7,
-    imageSrc: require('../Images/reviews/reviews7-min.png'),
-  },
-  {
-    id: 8,
-    imageSrc: require('../Images/reviews/reviews8-min.jpg'),
-  },
-  {
-    id: 11,
-    imageSrc: require('../Images/reviews/reviews11-min.jpg'),
-  },
-  {
-    id: 12,
-    imageSrc: require('../Images/reviews/reviews12-min.png'),
-  },
-  {
-    id: 13,
-    imageSrc: require('../Images/reviews/reviews13-min.png'),
-  },
-  {
-    id: 14,
-    imageSrc: require('../Images/reviews/reviews14-min.png'),
-  },
-  {
-    id: 15,
-    imageSrc: require('../Images/reviews/reviews15-min.jpg'),
-  },
-  {
-    id: 16,
-    imageSrc: require('../Images/reviews/reviews16-min.jpg'),
-  },
-  {
-    id: 17,
-    imageSrc: require('../Images/reviews/reviews17-min.jpg'),
-  },
-  {
-    id: 18,
-    imageSrc: require('../Images/reviews/reviews18-min.jpg'),
-  },
-  {
-    id: 19,
-    imageSrc: require('../Images/reviews/reviews19-min.jpg'),
-  },
-  {
-    id: 20,
-    imageSrc: require('../Images/reviews/reviews20-min.jpg'),
-  },
-  {
-    id: 21,
-    imageSrc: require('../Images/reviews/reviews21-min.jpg'),
-  },
-  {
-    id: 22,
-    imageSrc: require('../Images/reviews/reviews22-min.jpg'),
-  },
-  {
-    id: 23,
-    imageSrc: require('../Images/reviews/reviews23-min.jpg'),
-  },
+  // {
+  //   id: 2,
+  //   imageSrc: require('../Images/reviews/reviews2-min.png'),
+  // },
+  // {
+  //   id: 3,
+  //   imageSrc: require('../Images/reviews/reviews3-min.png'),
+  // },
+  // {
+  //   id: 4,
+  //   imageSrc: require('../Images/reviews/reviews4-min.png'),
+  // },
+  // {
+  //   id: 5,
+  //   imageSrc: require('../Images/reviews/reviews5-min.png'),
+  // },
+  // {
+  //   id: 6,
+  //   imageSrc: require('../Images/reviews/reviews6-min.png'),
+  // },
+  // {
+  //   id: 7,
+  //   imageSrc: require('../Images/reviews/reviews7-min.png'),
+  // },
+  // {
+  //   id: 8,
+  //   imageSrc: require('../Images/reviews/reviews8-min.jpg'),
+  // },
+  // {
+  //   id: 11,
+  //   imageSrc: require('../Images/reviews/reviews11-min.jpg'),
+  // },
+  // {
+  //   id: 12,
+  //   imageSrc: require('../Images/reviews/reviews12-min.png'),
+  // },
+  // {
+  //   id: 13,
+  //   imageSrc: require('../Images/reviews/reviews13-min.png'),
+  // },
+  // {
+  //   id: 14,
+  //   imageSrc: require('../Images/reviews/reviews14-min.png'),
+  // },
+  // {
+  //   id: 15,
+  //   imageSrc: require('../Images/reviews/reviews15-min.jpg'),
+  // },
+  // {
+  //   id: 16,
+  //   imageSrc: require('../Images/reviews/reviews16-min.jpg'),
+  // },
+  // {
+  //   id: 17,
+  //   imageSrc: require('../Images/reviews/reviews17-min.jpg'),
+  // },
+  // {
+  //   id: 18,
+  //   imageSrc: require('../Images/reviews/reviews18-min.jpg'),
+  // },
+  // {
+  //   id: 19,
+  //   imageSrc: require('../Images/reviews/reviews19-min.jpg'),
+  // },
+  // {
+  //   id: 20,
+  //   imageSrc: require('../Images/reviews/reviews20-min.jpg'),
+  // },
+  // {
+  //   id: 21,
+  //   imageSrc: require('../Images/reviews/reviews21-min.jpg'),
+  // },
+  // {
+  //   id: 22,
+  //   imageSrc: require('../Images/reviews/reviews22-min.jpg'),
+  // },
+  // {
+  //   id: 23,
+  //   imageSrc: require('../Images/reviews/reviews23-min.jpg'),
+  // },
 ];
